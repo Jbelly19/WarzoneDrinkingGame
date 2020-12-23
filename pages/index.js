36424851a94d6825fa86947d4632d68cbb1030d8
Josh Belmont <jbelmont19@live.com>
2020-12-22 18:47:26 -0800

    footer style

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,7 @@ const useStyles = makeStyles({
     textAlign: 'center',
   },
   footer: {
-    position: 'fixed',
-    bottom: '0px',
-    paddingTop: '20px',
-    width: '100%',
-    textAlign: 'center',
+    paddingTop: '30px',
   },
 });
 
@@ -36,31 +32,33 @@ export default function Index() {
           width={480}
         />
         <div>
-          <h1 className={classes.rules}>Rules:</h1>
-          <Typography variant='h6' align='center'>
-            Down Someone - Give Out 1 Drink
-          </Typography>
-          <Typography variant='h6' align='center'>
-            Bad Call Out - Take 1 Drink
-          </Typography>
-          <Typography variant='h6' align='center'>
-            Squad Wipe - Social (Everybody Drinks)
-          </Typography>
-          <Typography variant='h6' align='center'>
-            Run Over Someone in a Vehicle - Give Out 2 Drinks
-          </Typography>
-          <Typography variant='h6' align='center'>
-            Get Ran Over by a Vehicle - Take 2 Drinks
-          </Typography>
-          <Typography variant='h6' align='center'>
-            Win Gulag - Give Out 2 Drinks
-          </Typography>
-          <Typography variant='h6' align='center'>
-            Lose Gulag - Finish Drink Before End of Game or Bought Back
-          </Typography>
-          <Typography variant='h6' align='center'>
-            Fall to Your Death - Finish Your Drink
-          </Typography>
+          <div>
+            <h1 className={classes.rules}>Rules:</h1>
+            <Typography variant='h6' align='center'>
+              Down Someone - Give Out 1 Drink
+            </Typography>
+            <Typography variant='h6' align='center'>
+              Bad Call Out - Take 1 Drink
+            </Typography>
+            <Typography variant='h6' align='center'>
+              Squad Wipe - Social (Everybody Drinks)
+            </Typography>
+            <Typography variant='h6' align='center'>
+              Run Over Someone in a Vehicle - Give Out 2 Drinks
+            </Typography>
+            <Typography variant='h6' align='center'>
+              Get Ran Over by a Vehicle - Take 2 Drinks
+            </Typography>
+            <Typography variant='h6' align='center'>
+              Win Gulag - Give Out 2 Drinks
+            </Typography>
+            <Typography variant='h6' align='center'>
+              Lose Gulag - Finish Drink Before End of Game or Bought Back
+            </Typography>
+            <Typography variant='h6' align='center'>
+              Fall to Your Death - Finish Your Drink
+            </Typography>
+          </div>
         </div>
         <div className={classes.footer}>
           <Typography variant='subtitle2' align='center'>
